73de822a5303d770413a6f594230c9a08cb97050
Rishil Ramesh <rishil.ramesh@outlook.com>
2022-12-04 17:15:23 +0530

    Add : Add functions for day of the week and time

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import webbrowser
 import datetime
 import wikipedia
 
+id1 = r"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_DAVID_11.0"
+id2 = r"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
+
 
 # listen to the microphone and return the text
 def transform_audio_into_string():
@@ -58,7 +61,56 @@ def speak(message):
 
     # start engine
     engine = pyttsx3.init()
+    engine.setProperty("voice", id2)
 
     # deliver message
     engine.say(message)
     engine.runAndWait()
+
+
+# Ask day of the week
+def ask_day():
+
+    # integer to week name
+    weekly_calendar = {0: "Monday",
+                       1: "Tuesday",
+                       2: "Wednesday",
+                       3: "Thursday",
+                       4: "Friday",
+                       5: "Saturday",
+                       6: "Sunday"}
+
+    today = datetime.date.today()
+
+    # day of the week
+    week_day = today.weekday()
+    week_day = weekly_calendar[week_day]
+
+    # assistant speaks the day of the week
+    speak(f"Today is {week_day}!")
+
+
+# Inform what time it is now
+def ask_time():
+    
+    # Get the time
+    time = datetime.datetime.now()
+    hour = time.hour
+    minute = time.minute
+    noon = "A.M."
+
+    if hour > 12:
+        hour = hour % 12
+        noon = "P.M."
+
+    # assistant tells the time
+    message = f"Hey Rishil, at this moment, it is {hour}:{minute} {noon}!"
+    speak(message)
+
+
+# # ask_time()
+# engine = pyttsx3.init()
+#
+# for voice in engine.getProperty("voices"):
+#     print(voice)
+#     print(type(voice))
